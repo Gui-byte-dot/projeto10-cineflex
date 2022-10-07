c31a78b0f9c21e82e8a48ebf32cbffd9550f7097
guilherme <grgomes82@gmail.com>
2022-10-07 00:28:22 -0300

    first commit

--- src/Sessions.js
+++ src/Sessions.js
@@ -2,17 +2,22 @@ import axios from "axios"
 import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
 import React from 'react'
-
+import styled from "styled-components"
 export default function Sessions(){
-    const [session, setSession] = useState({})
+    const [sessions, setSessions] = useState([])
+    const [session, setSession] = useState([])
     const {imageId} = useParams()
 
 
     useEffect(() => {
         const promise = axios.get(`https://mock-api.driven.com.br/api/v5/cineflex/movies/${imageId}/showtimes`)
+      
         
         promise.then((res) => {
             setSession(res.data)
+            setSessions(res.data.days)
+            console.log(res.data.days)
+
         })
         promise.catch((err) => {
             console.log(err.response.data)
@@ -23,8 +28,88 @@ export default function Sessions(){
 
     return(
         <div>
-            {session.map(sess => <div key={sess.days.id}>{sess.days.weekday}</div>)}
+            <SelecionarHorario>
+                Selecionar o horário
+            </SelecionarHorario>
+            {sessions.map((reg) => 
+            <>
+                    <DataSemana key={reg.id}>
+                        <p>{reg.weekday} - {reg.date}</p>
+                    </DataSemana>
+                    <Horario>
+                        {reg.showtimes.map((exp) => <button key={exp.id}>{exp.name}</button>)}
+                    </Horario>              
+            </>)}
+            <RodapeFilme>
+                        <div>
+                        <p>{session.title}</p>
+                        <img src={session.posterURL} alt="rodape"/>
+                        </div>
+            </RodapeFilme>
+            
         </div>
             
     )
 }
+const SelecionarHorario = styled.div`
+    display:flex;
+    justify-content:center;
+    align-items:center;
+    height:110px;
+    width:100%;
+    font-size:24px;
+
+`
+const DataSemana = styled.div`
+    display: flex;
+    flex-direction:row;
+    margin-left:24px;
+    font-size:20px;
+`
+const Horario = styled.div`
+    display:flex;
+    flex-direction:row;
+    margin-left:23px;
+    button{
+        width: 83px;
+        height: 43px;
+        background: #E8833A;
+        border-radius: 3px;
+        font-size:18px;
+        color:white;
+        border:none;
+    }
+    button:nth-child(2){
+        margin-left:8px
+    }
+`
+const RodapeFilme = styled.footer`
+    width: 375px;
+    height: 117px;
+    left: 0px;
+    bottom: 0px;
+    background: #DFE6ED;
+    border: 1px solid #9EADBA;
+    flex-direction:row;
+
+
+    div{
+        display:flex;
+        flex-direction:row-reverse;
+        justify-content:left;
+        margin-left:10px;
+        align-items:center;
+        margin-top:14px;
+
+    }
+    img{
+        width: 64px;
+        height: 89px; 
+
+    }
+    p{
+        margin-left:14px;
+        font-size:26px;
+    }
+    
+`
